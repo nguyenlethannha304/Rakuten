9b1fa2166e08ba8774efa9aabd2c73d10f33a8a2
Nha Nguyen <nguyenlethanhnha304@gmail.com>
2022-09-07 06:16:35 +0700

    Code URLRakutenAdapter:

--- apps/rakuten/api/views.py
+++ apps/rakuten/api/views.py
@@ -1,30 +1,39 @@
 from http import HTTPStatus
 
 import requests
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from django.views import View
+from django.http import JsonResponse
+from apps.rakuten.utils import URLRakutenAdapter
+from django.conf import settings
 
 
-class RakutenSearchViewAPI(APIView):
-    permission_classes = []
-    authentication_classes = []
-
+class RakutenSearchViewAPI(View):
+    # START GET METHOD
     def get(self, request, *args, **kwargs):
-        search_url = self.get_search_url(request, *args, **kwargs)
-        rakuten_response = self.send_request_to_rakuten_api(search_url)
-        handled_response = self.handle_response(rakuten_response)
-        return Response(data=handled_response, status=HTTPStatus.OK)
+        enpoint = self.get_endpoint(request, *args, **kwargs)
+        rakuten_response = self.send_request_to_rakuten_api(enpoint)
+        if self.is_response_success(rakuten_response):
+            handled_response = self.handle_response(rakuten_response)
+            return JsonResponse(data=handled_response, status=HTTPStatus.OK)
+        return JsonResponse(data=rakuten_response.body, status=rakuten_response.status_code)
 
-    def get_search_url(self, request, *args, **kwargs):
-        pass
+    def get_endpoint(self, request, *args, **kwargs):
+        url = URLRakutenAdapter(request)
+        query_param_name_list = [
+            'applicationId', 'keyword', 'hits', 'page', 'minPrice', 'maxPrice']
+        return url.host + url.search_product_api + url.merge_query_params_to_string(query_param_name_list)
 
-    def send_request_to_rakuten_api(self, search_url):
+    def send_request_to_rakuten_api(self, enpoint):
         with requests.Session() as s:
-            response = s.get(search_url)
-            return response.json()
+            return s.get(enpoint)
+
+    def is_response_success(self, response):
+        if response.status_code:
+            pass
 
     def handle_response(self, rakuten_response):
         pass
+    # END GET METHOD
 
 
 rakuten_search_view_api = RakutenSearchViewAPI.as_view()
--- apps/rakuten/utils.py
+++ apps/rakuten/utils.py
@@ -1,44 +1,83 @@
 from django.conf import settings
 import re
-PRICE_FILTER_STRING_PATTERN = r'\[(\d+)\.\.(\d+)\]'
 
 
 class URLRakutenAdapter:
+    PRICE_FILTER_STRING_PATTERN = r'\d+\.\.\d+'
+
     def __init__(self, request, **kwargs):
         self.request = request
-        self.min_max_tuple = self.get_price_filter_string()
+        self.query_params = self.get_query_params()
+        # Min = min_max_list[0], Max = min_max_list[1]
+        self.min_max_list = self.get_min_max_from_filter_price()
+
+    def merge_query_params_to_string(self, list_params_to_merge):
+        rv = []
+        for param in list_params_to_merge:
+            if (value := getattr(self, param, None)):
+                rv.append(value)
+        return '?' + rv.join('&')
+
+    @property
+    def host(self):
+        if hasattr(settings, 'RAKUTEN_ENV'):
+            return settings.RAKUTEN_ENV
+        return 'https://app.rakuten.co.jp/services/api/'
+
+    @property
+    def search_product_api(self):
+        return 'Product/Search/20170426'
+
+    @property
+    def applicationId(self):
+        if hasattr(settings, 'RAKUTEN_ID'):
+            return settings.RAKUTEN_ID
+        return 'applicationId=1014989523353184170'
 
     @property
     def keyword(self):
-        if self.request.query_params.get('q', None):
-            return f"keyword={self.request.query_params.get('q')}"
+        # Từ khóa tìm kiếm
+        if self.query_params.get('q', None):
+            return f"keyword={self.query_params.get('q')}"
 
     @property
     def productId(self):
-        if self.request.query_params.get('', None):
+        if self.query_params.get('', None):
             pass
 
     @property
     def hits(self):
-        return 'hits='
+        # Số lượng sản phẩm trả về
+        if self.query_params.get('', None):
+            return f"hits={self.query_params.get('')}"
+        return "hits=10"
 
     @property
     def page(self):
-        if self.request.query_params.get('page', None):
-            return f"page={self.request.query_params.get('page')}"
+        if self.query_params.get('page', None):
+            return f"page={self.query_params.get('page')}"
+        return "page=1"
 
     @property
     def minPrice(self):
-        return 'minPrice='
+        if self.min_max_list:
+            return f"minPrice={self.min_max_list[0]}"
 
     @property
     def maxPrice(self):
-        return 'maxPrice='
+        if self.min_max_list:
+            return f"maxPrice={self.min_max_list[1]}"
 
     def get_min_max_from_filter_price(self):
-        filter_string = self.request.query_params('filter', None)
-        if filter_string:
-            match = re.match(PRICE_FILTER_STRING_PATTERN, filter_string)
+        filter_string = self.query_params('filter', None)
+        if filter_string and (match := re.search(self.PRICE_FILTER_STRING_PATTERN, filter_string)):
+            return match[0].split('..')
+
+    def get_query_params(self):
+        if hasattr(self.request, 'query_params'):
+            return self.request.query_params
+        if hasattr(self.request, 'GET'):
+            return self.request.GET
 
 
 class ProductRakutenAdapter:
